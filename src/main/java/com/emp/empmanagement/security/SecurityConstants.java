c1eba1f78329c5ec6dfb21a3d4f69b5cdec82232
Venkat Ramana Pittala <pvenkatramana25cool@gmail.com>
2025-12-14 20:17:12 +0530

    update sec constants comment

--- src/main/java/com/emp/empmanagement/security/SecurityConstants.java
+++ src/main/java/com/emp/empmanagement/security/SecurityConstants.java
@@ -1,6 +1,7 @@
 package com.emp.empmanagement.security;
 
 public class SecurityConstants {
+    //Set JWT expiration time as 8hrs and secret key
     public static final long JWT_Expiration = 28800000;
     public static final String JWT_Secret = "my_jwt_secret_is_very_long_and_secure_so_it_is_hard_to_guess_even_for_a_hacker";
 }
